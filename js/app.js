d37e1e6ffe5f1cbe2d38b62674ddf8903af9df53
“Erika <foglzerika@gmail.com>
2017-06-05 13:35:56 -0500

    Agregando mapa

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+  });
+    }
+
+var cargaPagina = function()
+{
+	 var map;
+      // initMap();
+
+}
+
+$(document).ready(cargaPagina);
