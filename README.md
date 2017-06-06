5d2708bac0640c5701514db5f33229de59208533
“Erika <foglzerika@gmail.com>
2017-06-05 23:41:54 -0500

    Agregando busqueda y modificando el README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # conAntojoMx
-App que hace uso de la api de localización
+Proyecto que tiene como objetivo el uso de la api de localización de Google maps.
+
+Version 0.1.0
+	Maquetado de la app, con diferentes elementos: mapa, búsqueda y lista de restaurantes
+Version 0.2.0
+	Creacion de un array con objetos de la lista de restaurantes 
+	Muestra de la lista de restaurantes en el HTML desde jQuery 
+	
--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 var arrayRestaurante= [];
 
-function Restaurante (nombre,foto,direccion,coordenadas) {
+function Restaurante (nombre,foto,direccion,coordenadas, tipoComida) {
 	this.nombre = nombre,
 	this.foto= foto,
 	this.direccion= direccion,
-	this.coordenadas = coordenadas
+	this.coordenadas = coordenadas,
+	this.tipoComida = tipoComida
 }
 
-var restaurante = new Restaurante("Terraza del Mayor", " https://goo.gl/XJRhRW","Calle República de Argentina 15 Planta Alta, Cuauhtemoc" ,{latitud: -99.1337437,longitud: 19.435756});
-var restaurante2 = new Restaurante("La  Barranca Valenciana","img/valenciana.jpg","Av. Centenario 91 C, Coyoacan, Del Carmen" ,{latitud: 19.3523942,longitud: -99.1656628});
-var restaurante3 = new Restaurante("Salon corona", "img/corona.jpg" , "Paseo de la Reforma 453, Cuauhtémoc",{latitud: 19.3523942,longitud: -99.1656628});
+var restaurante = new Restaurante("Terraza del Mayor", " https://goo.gl/XJRhRW","Calle República de Argentina 15 Planta Alta, Cuauhtemoc" ,{latitud: -99.1337437,longitud: 19.435756},"mexicana");
+var restaurante2 = new Restaurante("La  Barranca Valenciana","img/valenciana.jpg","Av. Centenario 91 C, Coyoacan, Del Carmen" ,{latitud: 19.3523942,longitud: -99.1656628}, "tortas");
+var restaurante3 = new Restaurante("Salon corona", "img/corona.jpg" , "Paseo de la Reforma 453, Cuauhtémoc",{latitud: 19.3523942,longitud: -99.1656628}, "tacos");
 arrayRestaurante.push(restaurante);
 arrayRestaurante.push(restaurante2);
 arrayRestaurante.push(restaurante3);
@@ -28,28 +29,46 @@ var plantillaRestaurant = "<div class = 'card horizontal'>"+
 						 		"</div>"+
 						 	"</div>";
 
-var mostrarContactos = function (contactos) {
+var mostrar = function (arreglo) {
 	var plantillaFinal = "";
-	arrayRestaurante.forEach(function (restaurante) {
-		plantillaFinal += plantillaRestaurant.replace("__nombre__", restaurante.nombre)
-											 .replace("__foto__", restaurante.foto)
-											 .replace("__direccion__", restaurante.direccion);
+	arreglo.forEach(function (elemento) {
+		plantillaFinal += plantillaRestaurant.replace("__nombre__", elemento.nombre)
+											 .replace("__foto__", elemento.foto)
+											 .replace("__direccion__", elemento.direccion);
 			
 	});
 	$("#listaRestaurant").html(plantillaFinal);
 };
 
+var busqueda = function (e){
+	e.preventDefault();
+	$itemBuscar = $("#icon_prefix").val();
 
+	var resultadosBusqueda = arrayRestaurante.filter(function(elemento){
 
-var cargarPagina = function () {
-	$("#get-location").click(obtenerUbicacion);
-	mostrarContactos();
-};
+		if($itemBuscar === elemento.tipoComida){
+			return elemento;
+		}
+	})
+	$mensaje = $("<h2>Mundo cruel x.X no hay restaurantes</h2>");
+	if(resultadosBusqueda.length ===0 )
+	{
+		$("#listaRestaurant").html($mensaje);
+	}else
+	{
+		mostrar(resultadosBusqueda);
+	}
+	
+	
+	console.log(resultadosBusqueda[0]);
+}
+
 
 
 var obtenerUbicacion = function (e) {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(mostrarPosicion);
+		
 	} else {
 		alert("Actualice su navegador");
 	}
@@ -73,6 +92,12 @@ var mostrarMapa = function (coordenadas) {
       map: map
     });
 }
+var cargarPagina = function () {
+	$("#get-location").click(obtenerUbicacion);
+	mostrar(arrayRestaurante);
+	$("form").submit(busqueda);
+};
+
 
 $(document).ready(cargarPagina);
 
